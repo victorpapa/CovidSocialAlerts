97d41a6bc603da0ad85cf45566d7628d5eccbcdd
victorpapa <vap32@cam.ac.uk>
2020-09-05 19:31:50 +0300

    Added one more trailing underscore.

--- backend/src/Data/DataRetriever.py
+++ backend/src/Data/DataRetriever.py
@@ -14,21 +14,37 @@ class DataRetriever:
     res_folder = None
     dataset = None
     dataset_folder = None
+    backend_folder = None
 
-    def __init__(self):
-        log_folder = os.path.join(os.getcwd(), "..", "..", "log")
+    def __setup_logs(self):
+        self.backend_folder = os.path.join(os.getcwd(), "..", "..")
+        log_folder_name = "log"
+        if log_folder_name not in os.listdir(self.backend_folder):
+            curr_dir = os.getcwd()
+            os.chdir(os.path.join(self.backend_folder))
+            os.makedirs(log_folder_name)
+            os.chdir(curr_dir)
+
+        log_folder = os.path.join(self.backend_folder, log_folder_name)
         log_file_name = "log.txt"
         if log_file_name not in os.listdir(log_folder):
             open(os.path.join(log_folder, log_file_name), "w+")
 
         logging.basicConfig(filename=os.path.join(log_folder, log_file_name), level=logging.DEBUG)
-        self.res_folder = os.path.join(os.getcwd(), "..", "..", "res")
+
+    def __init__(self):
+
+        self.__setup_logs()
+
+        self.res_folder = os.path.join(self.backend_folder, "res")
         self.social_network_file_name = os.path.join(self.res_folder, "facebook_combined.txt")
         self.social_network_file = open(self.social_network_file_name, "r")
         self.dataset = "facebook"
         self.dataset_folder = os.path.join(self.res_folder, self.dataset)
 
-    def add_nodes_and_edges(self):
+        self.__generate_graph()
+
+    def __add_nodes_and_edges(self):
 
         for line in self.social_network_file:
             ids = line.split()
@@ -44,14 +60,14 @@ class DataRetriever:
                                        work_location = -1)
             self.G.add_edge(ids[0], ids[1], co_worker = False)
 
-    def _get_users_with_egos(self):
+    def __get_users_with_egos(self):
         user_set = set()
         for file_name in os.listdir(self.dataset_folder):
             user_set.add(int(file_name.split(".")[0]))
 
         return user_set
 
-    def _get_featnames_for_user_ego(self, user_id):
+    def __get_featnames_for_user_ego(self, user_id):
         featnames = {}
 
         featnames_file_name = os.path.join(self.dataset_folder, str(user_id) + ".featnames")
@@ -66,7 +82,7 @@ class DataRetriever:
         return featnames
 
 
-    def _set_metadata_for_user_ego(self, ego_user_id, featnames):
+    def __set_metadata_for_user_ego(self, ego_user_id, featnames):
         
         egofeat_file_name = os.path.join(self.dataset_folder, str(ego_user_id) + ".egofeat")
         egofeat_file = open(egofeat_file_name, "r")
@@ -94,26 +110,26 @@ class DataRetriever:
 
         feat_file.close()
 
-    def _set_co_worker_metadata(self):
+    def __set_co_worker_metadata(self):
         for user_id1 in self.G.nodes:
             for user_id2 in self.G.neighbors(user_id1):
                 if self.G.nodes[user_id1]["work_location"] == self.G.nodes[user_id2]["work_location"] and                self.G.nodes[user_id1]["work_location"] != -1:
                     self.G.edges[user_id1, user_id2]["co_worker"] = True
 
-    def add_nodes_and_edges_metadata(self):
-        list_of_users_with_egos = self._get_users_with_egos()
+    def __add_nodes_and_edges_metadata(self):
+        list_of_users_with_egos = self.__get_users_with_egos()
 
         for user_id in list_of_users_with_egos:
-            featnames = self._get_featnames_for_user_ego(user_id)
-            self._set_metadata_for_user_ego(user_id, featnames)
+            featnames = self.__get_featnames_for_user_ego(user_id)
+            self.__set_metadata_for_user_ego(user_id, featnames)
 
-        self._set_co_worker_metadata()
+        self.__set_co_worker_metadata()
 
-    def generate_graph(self):
+    def __generate_graph(self):
         self.G = nx.Graph()
 
-        self.add_nodes_and_edges()
-        self.add_nodes_and_edges_metadata()
+        self.__add_nodes_and_edges()
+        self.__add_nodes_and_edges_metadata()
 
     def log_graph(self):
         for node in self.G.nodes.data():
@@ -127,6 +143,5 @@ class DataRetriever:
 
 if __name__ == "__main__":
     dataRetriever = DataRetriever()
-    dataRetriever.generate_graph()
     dataRetriever.log_graph()
     
